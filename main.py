9a5f22ffb4f1fabdf28613683f74dd19c7c713b2
dleinwan <dani.l.wander@gmail.com>
2023-03-28 15:48:44 -0400

    small change

--- main.py
+++ main.py
@@ -16,20 +16,7 @@ my_generation.generate_pattern()
 print("original: " + str(my_generation.pattern_original))
 print("grouped: " + str(my_generation.pattern_grouped))
 print(" ")
-# print(my_generation.pattern_with_octaves)
 
-#my_generation.play_pattern()
-
-###my_generation.play_pattern_groups_of_3()
-
-# time.sleep(.5)
-# my_generation.add_octaves()
-# my_generation.play_octaves()
-
-
-# print("original: " + str(my_generation.pattern_original))
-# print("grouped: " + str(my_generation.pattern_grouped))
-# print("with octaves: " + str(my_generation.pattern_with_octaves))
 my_generation.generate_and_load_to_file()
 
 print("BEFORE RANDOMIZATION")
@@ -46,9 +33,7 @@ my_generation.reload_original()
 print("AFTER RANDOMIZATION")
 print("original: " + str(my_generation.pattern_original))
 print("grouped: " + str(my_generation.pattern_grouped))
-#my_generation.play_pattern()
 print("")
 
-#my_generation.play_csv()
 
 my_generation.play_best_output()
